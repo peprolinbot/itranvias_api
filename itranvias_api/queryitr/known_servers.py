a85b892c05ffd0c4f2ff0c87fa6762c476fdaa4c
Gabriel Montes <64473570+Gabriel44200@users.noreply.github.com>
2025-04-25 23:19:30 +0200

    Add old itranvias endpoint
    On the function 0 it have different response

--- itranvias_api/queryitr/known_servers.py
+++ itranvias_api/queryitr/known_servers.py
@@ -2,3 +2,7 @@ ITRANVIAS_WEB = "https://itranvias.com/queryitr_v3.php"
 """
 `queryitr_v3.php` endpoint used in the itranvias web app (https://itranvias.com/)
 """
+ITRANVIAS_OLD = "https://itranvias.com/queryitr.php"
+"""
+`queryitr.php` endpoint used in the old itranvias app
+"""
