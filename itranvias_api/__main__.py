ae7eb9068285ae6b1e3064ba3ff29d6831e77928
Pedro Rey Anca <personal@peprolinbot.com>
2025-05-13 17:08:50 +0200

    Make itranvias-cli use the new cached implementation

--- itranvias_api/__main__.py
+++ itranvias_api/__main__.py
@@ -1,21 +1,16 @@
 import argparse
-import itranvias_api.queryitr as api
+import itranvias_api.queryitr.cached as api
 
-STATIC_DATA = api.info.get_general_info()
-
-
-def line_id_to_name(line_id: int) -> str:
-    return STATIC_DATA["lines"][line_id].name
-
-
-def stop_id_to_name(stop_id: int) -> str:
-    return STATIC_DATA["stops"][stop_id].name
-
-
-def display_next_buses(buses_data: dict) -> None:
-    if buses_data:
-        for line_id, buses in buses_data.items():
-            print(f"Line {line_id_to_name(line_id)}:")
+def display_stop_next_buses(stop_id:int) -> None:
+    stop=api.models.Stop.get(stop_id)
+    print(f"Buses for {stop.name} ({stop.id}):\n")
+    
+    data = stop.get_next_buses()
+    if data:
+        for line_data in data.values():
+            line = line_data["line"]
+            buses = line_data["buses"]
+            print(f"Line {line.name}:")
             for bus in buses:
                 print(
                     f"{" "*4}- 🚍 {bus.id:>3} | 📍 {bus.distance:<6} m | ⌛ {bus.time:>2} minutes"
@@ -24,26 +19,45 @@ def display_next_buses(buses_data: dict) -> None:
         print("It looks like there are no buses for this stop")
 
 
-def display_route_stops_and_buses(route: api.models.Route) -> None:
+def display_line_stops_and_buses(line_id:int,route_id:int) -> None:
+    line = api.models.Line.get(line_id)
+    routes=line.get_buses()
+    route_data = routes[route_id]
+    route = route_data["route"]
+    stops_data=route_data["stops"]
+
+    def bus_str(bus: api.models.RTBus) -> str:
+        """Get a bus' string representation"""
+        return f"[🚍 {bus.id}]"
+
+    def stop_str(stop: api.models.Stop) -> str:
+        """Get a stop's string representation"""
+        return f"[🚏 {stop.id} - {stop.name}]"
+
+    def print_separator()->None:
+        """Print the `|` between lines"""
+        print(f"{" "*4}|")
+
     for stop in route.stops:
-        stop_str = f"[🚏 {stop.id} - {stop_id_to_name(stop.id)}]"
-        buses_str = ""
+        stop_data = stops_data.get(stop.id)
 
-        # Check for buses at this stop or have this as last stop
-        buses_for_stop = route.buses.get(stop.id, [])
+        if stop_data is None:
+            print(stop_str(stop))
+            print_separator()
+            continue
 
-        for bus in buses_for_stop:
-            bus_str = f"[🚍 {bus.id}]"
+        stop = stop_data["stop"]
+        buses = stop_data["buses"]
 
-            if bus.at_stop:
-                # We add the bus to the left of the stop string
-                stop_str = f"{bus_str} - {stop_str}"
-            else:
-                buses_str += f"{bus_str}\n{" "*4}|"
+        for bus in buses["at_stop"]:
+            print(f"{bus_str(bus)} - ", end="")
 
-        print(f"{stop_str}\n{" "*4}|")
-        if buses_str != "":
-            print(buses_str)
+        print(stop_str(stop))
+        print_separator()
+
+        for bus in buses["moving"]:
+            print(bus_str(bus))
+            print_separator()
 
 
 def main() -> None:
@@ -71,18 +85,12 @@ def main() -> None:
 
     args = parser.parse_args()
 
+    api.database.default_db.update() # It's fast after the first time
+
     if args.command == "stop":
-        print(
-            f"Buses for stop {args.stop_id} ({
-              stop_id_to_name(args.stop_id)}):\n"
-        )
-        next_buses = api.stops.get_stop_buses(args.stop_id)
-        display_next_buses(next_buses)
+        display_stop_next_buses(args.stop_id)
     elif args.command == "line":
-        routes = api.lines.get_line_buses(args.line_id)
-        route = routes[args.route_id]
-        route.stops = STATIC_DATA["lines"][args.line_id].routes[args.route_id].stops
-        display_route_stops_and_buses(route)
+        display_line_stops_and_buses(args.line_id, args.route_id)
 
 
 if __name__ == "__main__":
